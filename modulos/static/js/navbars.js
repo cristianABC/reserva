f5bfec26c749821b1fd9a66c05d3f2d4a3c790bd
LuisMesa <la.mesa10@uniandes.edu.co>
2017-08-26 00:18:21 -0500

    Detail modal is now working :D

--- modulos/static/js/navbars.js
+++ modulos/static/js/navbars.js
@@ -1,8 +1,18 @@
-function openNav(id) {
+function openNav(idModal, idSpecie) {
     // document.getElementById("loginSideNav").style.width = "30vw";
-    $('#'+id).css('width','30vw');
+    let modal = $('#' + idModal);
+    if(idSpecie || idSpecie === 0) {
+        modal.children('iframe').remove();
+        modal.append('<iframe  frameborder=\"0\"></iframe>');
+        modal.children('iframe').attr('src', './detalle/' + idSpecie);
+        modal.children('iframe').ready(()=>{modal.css('width','30vw');})
+    }
+    else {
+        modal.css('width', '30vw');
+    }
 }
 
 function closeNav() {
     $('.sidenav').css('width','0');
+    $('#detailSideNav').children('iframe').remove();
 }
